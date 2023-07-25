766c0e2404d5702ef29b7d96dc5aa99135aaeeb4
dhruv <dev@dhruv.it>
2023-07-25 17:46:11 +0530

    Request card data from list

--- js/client.js
+++ js/client.js
@@ -1,12 +1,9 @@
 /* global TrelloPowerUp */
-
 var Promise = TrelloPowerUp.Promise;
-
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 window.TrelloPowerUp.initialize({
 	'list-actions': function (t) {
-		return t.list('name', 'id')
+		return t.list('id', 'name', 'cards')
 			.then(function (list) {
 				return [{
 					text: "Get Pending Payment List",
